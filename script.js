08db76d124fd6a72fd6eb62eb15d62111125e3e9
Michael <100490812+DebSol11@users.noreply.github.com>
2025-06-25 16:42:35 +0200

    Make input field and button work together
    For increased user experience the gird can now be adjusted by typing in a
    number in the input field, followed by a set size button press.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function populateBoard(size) {
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  for (let i = 0; i < 256; i++) {
+  for (let i = 0; i < size*size; i++) {
     let square = document.createElement("div");
     square.style.backgroundColor = "blue";
     board.insertAdjacentElement("beforeend", square);
@@ -11,3 +11,17 @@ function populateBoard(size) {
 }
 
 populateBoard(16);
+
+function changeSize(input) {
+    populateBoard(input);
+}
+
+// Use input field to call the changeSize function with it's input value
+
+let inputField = document.querySelector("input");
+inputField.style.color = "green";
+
+let setSizeButton = document.querySelector(".set-size");
+setSizeButton.addEventListener("click", () => {changeSize(inputField.value)});
+
+
