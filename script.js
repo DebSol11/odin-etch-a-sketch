c6287a06abf4a1b0bc5e487bf78ae2dd3e43f01f
Michael <100490812+DebSol11@users.noreply.github.com>
2025-06-27 22:59:50 +0200

    Change .onclick to eventListeners
    This is preferred coding style because it allows for multiple events
    to be performed on a single object.

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+// global Variables
+let color = "black";
+
+const blackButton = document.querySelector("#black");
+const eraserButton = document.querySelector("#eraser");
+const greyButton = document.querySelector("#grey");
+
+greyButton.addEventListener("click", () => {
+  color = "grey";
+});
+eraserButton.addEventListener("click", () => {
+  color = "white";
+});
+blackButton.addEventListener("click", () => {
+  color = "black";
+});
+
 function populateBoard(size) {
   let board = document.querySelector(".board");
+  // remove all squares from the DOM before populating new
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  for (let i = 0; i < size*size; i++) {
+  for (let i = 0; i < size * size; i++) {
     let square = document.createElement("div");
-    square.style.backgroundColor = "blue";
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
     board.insertAdjacentElement("beforeend", square);
   }
 }
@@ -13,15 +35,25 @@ function populateBoard(size) {
 populateBoard(16);
 
 function changeSize(input) {
+  if (input >= 2 && input <= 100) {
     populateBoard(input);
+  } else {
+    console.log("Please type in a number between 2 and 100");
+  }
 }
 
-// Use input field to call the changeSize function with it's input value
-
 let inputField = document.querySelector("input");
-inputField.style.color = "green";
 
 let setSizeButton = document.querySelector(".set-size");
-setSizeButton.addEventListener("click", () => {changeSize(inputField.value)});
+setSizeButton.addEventListener("click", () => {
+  changeSize(inputField.value);
+});
 
+function colorSquare() {
+  // this here refers to whatever div we added the eventListener to
+  this.style.backgroundColor = color;
+}
 
+function changeColor(choice) {
+  color = choice;
+}
